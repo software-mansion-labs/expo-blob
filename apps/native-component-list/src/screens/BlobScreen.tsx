5a46ea3865a607cb1088985e0209e7f2dc7dc91c
Artur Gęsiarz <artur.gesiarz@swmansion.com>
2025-07-18 19:28:23 +0200

    MonoText

--- apps/native-component-list/src/screens/BlobScreen.tsx
+++ apps/native-component-list/src/screens/BlobScreen.tsx
@@ -1,31 +1,15 @@
+import MonoText from '../components/MonoText';
 import { Page, Section } from '../components/Page';
-import { ArrayBufferBlobTest } from './Blob/ArrayBufferBlobTest';
 import { CreateBlobTestComponent } from './Blob/CreateBlobTest';
-import { SliceBlobTestComponent } from './Blob/SliceBlobTest';
-import { StreamBlobTestComponent } from './Blob/StreamBlobTest';
 import { TestContainer } from './Blob/TestContainer';
 
 export default function BlobScreen() {
   return (
     <Page>
       <Section title="Create Blob">
+        <MonoText>Test Create Blob</MonoText>
         <TestContainer title="Create Blob Test">
           <CreateBlobTestComponent />
-        </TestContainer>
-      </Section>
-      <Section title="Slice Blob">
-        <TestContainer title="Slice Blob Test">
-          <SliceBlobTestComponent />
-        </TestContainer>
-      </Section>
-      <Section title="Stream Blob">
-        <TestContainer title="Stream Blob Test">
-          <StreamBlobTestComponent />
-        </TestContainer>
-      </Section>
-      <Section title="ArrayBuffer/Bytes Blob">
-        <TestContainer title="ArrayBuffer/Bytes Blob Test" disabled>
-          <ArrayBufferBlobTest />
         </TestContainer>
       </Section>
     </Page>
